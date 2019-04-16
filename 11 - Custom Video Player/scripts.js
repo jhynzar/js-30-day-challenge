0a68425f36d931fabef7d035ec5c62a2a80c1486
jhynzar <jhynzar@gmail.com>
2019-04-16 09:51:34 +0800

    Sliders Functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,6 +3,7 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
+const sliders = player.querySelectorAll('.player__slider');
 
 //Build functions
 function togglePlay() {
@@ -17,6 +18,10 @@ function skip(){
     video.currentTime += Number(this.dataset.skip);
 }
 
+function handleSliders() {
+    video[this.name] = this.value;
+}
+
 //Bind Listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -24,6 +29,6 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 
-skipButtons.forEach(element => {
-    element.addEventListener('click', skip);
-});
+skipButtons.forEach(element => element.addEventListener('click', skip));
+
+sliders.forEach(element => element.addEventListener('change', handleSliders));
