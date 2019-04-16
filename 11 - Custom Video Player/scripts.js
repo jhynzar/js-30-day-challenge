e63042cc63b780036704726e831924051a43f53f
jhynzar <jhynzar@gmail.com>
2019-04-16 09:45:43 +0800

    Skip functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
 //Build functions
 function togglePlay() {
@@ -12,9 +13,17 @@ function updateButton() {
     toggle.textContent = (this.paused === true) ? '►' : '❚ ❚';
 }
 
+function skip(){
+    video.currentTime += Number(this.dataset.skip);
+}
+
 //Bind Listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(element => {
+    element.addEventListener('click', skip);
+});
